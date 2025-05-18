83f3a482656161de35186c3b4223447bb75c01b8
Eirikr Hinngart <151315375+Oichkatzelesfrettschen@users.noreply.github.com>
2025-05-18 03:00:35 -0700

    Update build-modules.sh to detect kernel version

--- build-modules.sh
+++ build-modules.sh
@@ -3,10 +3,11 @@
 echo "Building ./modules/ floppy folder..."
 
 rm -rf ./modules
+kernel_version=$(make -s -C linux-5.17.2 kernelrelease)
 INSTALL_MOD_PATH=$(pwd)/modules make -C linux-5.17.2 modules_install
-mv modules/lib/modules/* modules/
+mv "modules/lib/modules/${kernel_version}" modules/
 rmdir modules/lib/modules modules/lib
-rm modules/5.17.2clyne/{source,build}
+rm modules/${kernel_version}/{source,build}
 find modules/ -type f -name "*.ko" -exec strip --strip-debug {} \;
 du -sh ./modules
 
